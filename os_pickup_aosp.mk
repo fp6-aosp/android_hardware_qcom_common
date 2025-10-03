57e2b5d68813341bd628eb5ed9eb40f6e064f533
Tim Zimmermann <tim@linux4.de>
2025-10-03 11:54:07 +0200

    qcom: Remove unused os_pickup files
    Change-Id: Id21ad6920e8daca59d87645c15bb7e3fbd351162

--- os_pickup_aosp.mk
+++
@@ -1,8 +0,0 @@
-# This file guards most of hardware/qcom/* from
-# being used on devices which use hardware/qcom-caf/*
-
-LOCAL_PATH := $(call my-dir)
-
-ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
-include $(call first-makefiles-under,$(LOCAL_PATH))
-endif
